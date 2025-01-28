9082b9dba88007971c4b70779934417a40a2a339
Hectordev4 <hector.valverde.cana@gmail.com>
2025-01-28 09:54:49 +0100

    Refactor data fetching functions to use a mock API and improve error handling

--- frontend/pronunciationAppFront/src/data-api.js
+++ frontend/pronunciationAppFront/src/data-api.js
@@ -1,6 +1,18 @@
 import axios from "axios";
 
-//export for words
+
+const BASE_URL = "https://2c7c4ef1-dffa-4192-b088-84ffd5f4291d.mock.pstmn.io";
+
+export const fetchWords = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/words`);
+    return response.data.words;
+  } catch (error) {
+    console.error("Error fetching words:", error);
+    throw error;
+  }
+};
+/*export for words
 export const fetchWords = async () => {
   try {
     const response = await axios.get("./src/assets/100_words.json");
@@ -9,9 +21,18 @@ export const fetchWords = async () => {
     console.error("Error fetching words:", error);
     throw error;
   }
-}
+}*/
 
-//export for users
+export const fetchUser = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/user`);
+    return response.data[0]; // Return the first user from the array
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    throw error;
+  }
+};
+/*export for users
 export const fetchUser = async () => {
   try {
     const response = await axios.get("./src/assets/users.json");
@@ -20,4 +41,4 @@ export const fetchUser = async () => {
     console.error("Error fetching user:", error);
     throw error;
   }
-};
+};*/
