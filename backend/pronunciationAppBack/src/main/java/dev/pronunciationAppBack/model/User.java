e09b4ead62cd513a69e6606c9586724969314387
Luiscuatro <luisotonico@gmail.com>
2025-02-01 11:46:58 +0100

    feat: add User entity to the project

---
+++ backend/pronunciationAppBack/src/main/java/dev/pronunciationAppBack/model/User.java
@@ -0,0 +1,73 @@
+package dev.pronunciationAppBack.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private boolean isActive;
+
+    public User() {}
+
+    public User(String id, String name, String email, String password, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.isActive = isActive;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean setActive() {
+        return isActive;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+        "id='" + id + '\'' +
+        ", name='" + name + '\'' +
+        ", email='" + email + '\'' +
+        ", isActive=" + isActive +
+        '}';
+    }
+
+    
+}
