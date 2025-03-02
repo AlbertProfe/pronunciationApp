18094935ca0e8062381884f0101a75761a17a174
marcius02 <marcfernandezz1011@gmail.com>
2025-03-02 19:27:26 +0100

    feat: set up Postman mock server and implement axios function for data fetching

--- frontend/pronunciationAppFront/src/data-api.js
+++ frontend/pronunciationAppFront/src/data-api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from "axios";
 
-const BASE_URL = "https://a387bb02-2aa0-41a6-b8f7-9cc5247b9d5f.mock.pstmn.io";
+const BASE_URL = "https://c6e298f1-9bc6-4008-b85e-684c309e8250.mock.pstmn.io";
 
 export const fetchWords = async () => {
   try {
@@ -12,3 +12,13 @@ export const fetchWords = async () => {
     throw error;
   }
 };
+
+export const fetchUser = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/user`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    throw error;
+  }
+}
